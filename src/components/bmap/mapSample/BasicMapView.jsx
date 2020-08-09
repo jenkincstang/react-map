726dd717234649592d5060161910ec8723aa7815
jenkincstang <jenkincs@163.com>
2020-08-09 20:05:12 +0800

    [react-map][jenkin]test: add some test baidu map api

--- src/components/bmap/mapSample/BasicMapView.jsx
+++ src/components/bmap/mapSample/BasicMapView.jsx
@@ -10,7 +10,36 @@ export default class BasicMapView extends Component{
     componentDidMount(){
         console.log(window);
         let map =new BMap.Map("allmap");
-        map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);  // 初始化地图,设置中心点坐标和地图级别
+
+        var p1 = new BMap.Point(116.301934,39.977552);
+        var p2 = new BMap.Point(116.508328,39.919141);
+
+        var driving = new BMap.DrivingRoute(map, {renderOptions:{map: map, autoViewport: true}});
+        driving.search(p1, p2);
+
+        var local = new BMap.LocalSearch(map, {
+            renderOptions:{map: map}
+        });
+        local.search("停车场");
+        map.centerAndZoom(new BMap.Point(116.301934,39.977552), 11);  // 初始化地图,设置中心点坐标和地图级别
+
+
+
+
+        var point = new BMap.Point(116.404, 39.915);
+        var marker = new BMap.Marker(point);
+        marker.addEventListener("click", function(){
+            alert("您点击了标注");
+        });
+        var content = "中心位置";
+        var label = new BMap.Label(content, {       // 创建文本标注
+            position: point,                          // 设置标注的地理位置
+            //offset: new BMap.Size(10, 20)           // 设置标注的偏移量
+        })
+        map.addOverlay(label);
+
+
+        map.addOverlay(marker);
         //添加地图类型控件
         map.addControl(new BMap.MapTypeControl({
             mapTypes:[
